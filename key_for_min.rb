ccedf381ce45703b91d0344461999575d62471d0
Keith Williams <thestreetkw@hotmail.com>
2020-10-14 02:00:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
 smallest_key = nil 
-smallest_value = Infinity 
+smallest_value = INFINITY 
 name_hash.each do |key, value|
 if value < smallest_value 
   smallest_value = value 
